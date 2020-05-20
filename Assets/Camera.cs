54bc1c7f871ff4a47f4a532583f798f48633b854
Alexandre Cateli <48059013+catelii@users.noreply.github.com>
2020-05-20 18:22:25 -0300

    Cenario Aberto

---
+++ Assets/Camera.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour {
+
+    public bool Mouse = true;
+    public float Sensibilidade = 2.0f;
+
+    private float mouseX = 0.0f, mouseY = 0.0f;
+
+
+    void Start () {
+
+        if (!Mouse)
+        {
+            return;
+        }
+
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        mouseX += Input.GetAxis("Mouse X") * Sensibilidade;
+        mouseY -= Input.GetAxis("Mouse Y") * Sensibilidade;
+
+        transform.eulerAngles = new Vector3(mouseY, mouseX, 0);
+    }
+}
---
+++ Assets/walk.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class walk : MonoBehaviour {
+
+    public float Vertical;
+    public float Horizontal;
+
+	void Start () {
+      
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        //float translate = (Input.GetAxis ("Vertical") * _velocidade) * Time.deltaTime;
+        //transform.Translate (0, 0, translate);
+        print("Vertical" + Input.GetAxis("Vertical"));
+        print("Horizontal" + Input.GetAxis("Horizontal"));
+
+        float translate = (Input.GetAxis("Vertical") * Vertical)* Time.deltaTime;
+        float rotation = (Input.GetAxis("Horizontal") * Horizontal) * Time.deltaTime;
+        transform.Translate(rotation, 0, translate);
+    }
+}
